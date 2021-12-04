de26351d9d472322da6cdc666630517714714f8c
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-12-04 20:54:12 +0100

    3.36 "Doctrine crud - update" finished

--- 02-basics/src/Controller/DefaultController.php
+++ 02-basics/src/Controller/DefaultController.php
@@ -18,12 +18,16 @@ class DefaultController extends AbstractController
     #[Route('/home', name: 'home')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $repository = $doctrine->getRepository(User::class);
-        //$user = $repository->find(1);
-        //$user = $repository->findOneBy(['name' => 'Robert', 'id' => 6]);
-        //$users = $repository->findBy(['name' => 'Robert'], ['id' => 'DESC'] );
-        $users = $repository->findAll();
-        dump($users);
+        $entityManager = $doctrine->getManager();
+
+        $id = 1;
+        $user = $entityManager->getRepository(User::class)->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException("no user found");
+        }
+        $user->setName("NewUserName");
+        $entityManager->flush();
+        dump($user);
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController'
         ]);
