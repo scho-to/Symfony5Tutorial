cc1b748d7bc85d5b013b18ba09136a5e2fb5521d
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-12-11 21:09:02 +0100

    3.60 "Cache - basic usage" finished

--- 02-basics/src/Controller/DefaultController.php
+++ 02-basics/src/Controller/DefaultController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
-use App\Services\ServiceInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(ManagerRegistry $doctrine, ServiceInterface $si): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
+        $cache = new FilesystemAdapter();
+        $posts = $cache->getItem("database.get_posts");
+        if(!$posts->isHit()){
+            $posts_from_db = ['post 1', 'post 2', 'post 3'];
+            dump("connected to db");
+
+            $posts->set(serialize($posts_from_db));
+            $posts->expiresAfter(5);
+            $cache->save($posts);
+        }
+        //$cache->deleteItem("database.get_posts");
+        $cache->clear();
+        dump(unserialize($posts->get()));
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController'
