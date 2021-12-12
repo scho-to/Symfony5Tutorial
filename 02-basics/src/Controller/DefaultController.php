8387b25077e510d3d0b5fa029a4d2b2289277339
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-12-12 21:08:36 +0100

    3.67 "Create listener" finished

--- 02-basics/src/Controller/DefaultController.php
+++ 02-basics/src/Controller/DefaultController.php
@@ -4,68 +4,17 @@ namespace App\Controller;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use Symfony\Component\Cache\Adapter\TagAwareAdapter;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(Request $request, ManagerRegistry $doctrine)
     {
         $entityManager = $doctrine->getManager();
-        $cache = new TagAwareAdapter(
-            new FilesystemAdapter()
-        );
 
-        $acer = $cache->getItem("acer");
-        $dell = $cache->getItem("dell");
-        $ibm = $cache->getItem("ibm");
-        $apple = $cache->getItem("apple");
-
-        if(!$acer->isHit())
-        {
-            $acer_from_db = "acer laptop";
-            $acer->set($acer_from_db);
-            $acer->tag(["computers", "laptops", "acer"]);
-            $cache->save($acer);
-            dump("acer laptop from database ...");
-        }
-        if(!$dell->isHit())
-        {
-            $dell_from_db = "acer laptop";
-            $dell->set($dell_from_db);
-            $dell->tag(["computers", "laptops", "dell"]);
-            $cache->save($dell);
-            dump("dell laptop from database ...");
-        }
-        if(!$ibm->isHit())
-        {
-            $ibm_from_db = "ibm desktop";
-            $ibm->set($ibm_from_db);
-            $ibm->tag(["computers", "desktops", "ibm"]);
-            $cache->save($ibm);
-            dump("ibm desktop from database ...");
-        }
-        if(!$apple->isHit())
-        {
-            $apple_from_db = "apple desktop";
-            $apple->set($apple_from_db);
-            $apple->tag(["computers", "desktops", "apple"]);
-            $cache->save($apple);
-            dump("apple desktop from database ...");
-        }
-
-        //$cache->invalidateTags(["ibm"]);
-        //$cache->invalidateTags(["desktops"]);
-        //$cache->invalidateTags(["laptops"]);
-        $cache->invalidateTags(["computers"]);
-
-        dump($acer->get());
-        dump($dell->get());
-        dump($ibm->get());
-        dump($apple->get());
+        dump($request, $this);
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController'
