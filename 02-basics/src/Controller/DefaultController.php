11507a73fafb963dee41639909857e55940d5396
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-12-18 12:06:26 +0100

    3.80 "Email - install email package & send an email" finished

--- 02-basics/src/Controller/DefaultController.php
+++ 02-basics/src/Controller/DefaultController.php
@@ -2,52 +2,36 @@
 
 namespace App\Controller;
 
-use App\Entity\Video;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use App\Events\VideoCreatedEvent;
-use stdClass;
-use App\Form\VideoFormType;
-use DateTime;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
-    private ?EventDispatcherInterface $dispatcher = null;
-    public function __construct(EventDispatcherInterface $dispatcher)
-    {
-        $this->dispatcher = $dispatcher;
-    }
-
     #[Route('/home', name: 'home')]
-    public function index(ManagerRegistry $doctrine, Request $request)
+    public function index(ManagerRegistry $doctrine, Request $request, Swift_Mailer $mailer)
     {
         $entityManager = $doctrine->getManager();
-        $videos = $entityManager->getRepository(Video::class)->findAll();
-        dump($videos);
-        $video = new Video();
-        //$video->setTitle("Wrtie a blog post");
-        //$video->setCreatedAt(new DateTime("tomorrow"));
-        //$video = $entityManager->getRepository(Video::class)->find(1);
 
-        $form = $this->createForm(VideoFormType::class, $video);
-        $form->handleRequest($request);
-        
-        if ($form->isSubmitted() && $form->isValid()) { 
-            $file = $form->get('file')->getData();
-            $fileName = sha1(random_bytes(14)).'.'.$file->guessExtension();
-            $file->move($this->getParameter('videos_directory'), $fileName);
-            $video->setFile($fileName);
-            $entityManager->persist($video);
-            $entityManager->flush();
-            return $this->redirectToRoute("home");
-        }
+        $message = (new Swift_Message('Hello Email'))
+            ->setFrom("send@example.com")
+            ->setTo("receipant@receive.com")
+            ->setBody(
+                $this->renderView(
+                    'emails/registration.html.twig',
+                    ['name' => 'Robert']
+                ),
+                'text/html'
+            )
+        ;
+
+        $mailer->send($message);
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
-            'form' => $form->createView()
         ]);
     }
 }
