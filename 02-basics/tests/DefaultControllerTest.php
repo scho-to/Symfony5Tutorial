f4d1ee60545b06cfacd7bb41ed2bf833a45ae79d
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-12-29 21:57:07 +0100

    3.100 "Functional tests - click links" finished

--- 02-basics/tests/DefaultControllerTest.php
+++ 02-basics/tests/DefaultControllerTest.php
@@ -10,31 +10,14 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/home');
+        $link = null;
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Hello');
 
-        $this->assertGreaterThan(
-            0,
-            $crawler->filter("html:contains('Hello')")->count()
-        );
-        // $this->assertGreaterThan(0, $crawler->filter("h1.class")->count());
-        $this->assertCount(1,$crawler->filter("h1"));
-        $this->assertTrue(
-            $client->getResponse()->headers->contains(
-                'Content-Type',
-                'application/json'
-            ),
-            'the "Content-Type" header is "application/json"'
-        );
-        $this->assertStringContainsString("DefaultController", $client->getResponse()->getContent());
-        $this->assertMatchesRegularExpression('/foo(bar)?/', $client->getResponse()->getContent());
-        $this->assertTrue($client->getResponse()->isSuccessful(), "response status i 2xx");
-        $this->assertTrue($client->getResponse()->isNotFound());
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue(
-            $client->getResponse()->isRedirect("/demo/contact")
-        );
-        $this->assertTrue($client->getResponse()->isRedirect());
+        $link = $crawler->filter("a:contains('Have an account?')")->link();
+        $crawler = $client->click($link);
+
+        $this->assertStringContainsString("Keep me logged in", $client->getResponse()->getContent());
     }
 }
