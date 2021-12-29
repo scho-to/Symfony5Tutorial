fed0bb2c094b2028556f1c2b384efdc233629f76
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-12-29 21:48:29 +0100

    3.99 "Functional & unit tests - common assertions" finished

--- 02-basics/tests/DefaultControllerTest.php
+++ 02-basics/tests/DefaultControllerTest.php
@@ -13,5 +13,28 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Hello');
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter("html:contains('Hello')")->count()
+        );
+        // $this->assertGreaterThan(0, $crawler->filter("h1.class")->count());
+        $this->assertCount(1,$crawler->filter("h1"));
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+        $this->assertStringContainsString("DefaultController", $client->getResponse()->getContent());
+        $this->assertMatchesRegularExpression('/foo(bar)?/', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->isSuccessful(), "response status i 2xx");
+        $this->assertTrue($client->getResponse()->isNotFound());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue(
+            $client->getResponse()->isRedirect("/demo/contact")
+        );
+        $this->assertTrue($client->getResponse()->isRedirect());
     }
 }
