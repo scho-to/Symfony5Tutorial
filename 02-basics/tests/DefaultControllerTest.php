4577fe9c6f46835331c49957413c854e6c566950
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-03 11:28:02 +0100

    3.102 "Functional tests - data providers" finished

--- 02-basics/tests/DefaultControllerTest.php
+++ 02-basics/tests/DefaultControllerTest.php
@@ -6,34 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    /**
+     * @dataProvider provideUrls
+     */
+    public function testSomething($url): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $link = null;
+        $crawler = $client->request('get', $url);
 
         $this->assertTrue($client->getResponse()->isSuccessful());
-        $this->assertEquals(1, $crawler->filter("label:contains('Email:')")->count());
-        $this->assertEquals(1, $crawler->filter("form")->count());
 
-        $form = $crawler->selectButton("login")->form();
-        $form['email'] = "user@user.com";
-        $form["password"] = "passw";
+    }
 
-        $crawler = $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirection());
-        $crawler = $client->followRedirect();
-
-        $this->assertEquals(1, $crawler->filter('a:contains("Logout")')->count());
-
-
-
-        // $this->assertResponseIsSuccessful();
-        // $this->assertSelectorTextContains('h1', 'Hello');
-
-        // $link = $crawler->filter("a:contains('Have an account?')")->link();
-        // $crawler = $client->click($link);
-
-        // $this->assertStringContainsString("Keep me logged in", $client->getResponse()->getContent());
+    public function provideUrls()
+    {
+        return [
+            ['/home'],
+            ['/login']
+        ];
     }
 }
