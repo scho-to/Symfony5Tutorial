260b7834455f3ee8766980a77dd8fc56fdc58ebc
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-03 11:14:47 +0100

    3.101 "Functional tests - send a form" finished

--- 02-basics/tests/DefaultControllerTest.php
+++ 02-basics/tests/DefaultControllerTest.php
@@ -9,15 +9,31 @@ class DefaultControllerTest extends WebTestCase
     public function testSomething(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/home');
+        $crawler = $client->request('GET', '/login');
         $link = null;
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter("label:contains('Email:')")->count());
+        $this->assertEquals(1, $crawler->filter("form")->count());
 
-        $link = $crawler->filter("a:contains('Have an account?')")->link();
-        $crawler = $client->click($link);
+        $form = $crawler->selectButton("login")->form();
+        $form['email'] = "user@user.com";
+        $form["password"] = "passw";
 
-        $this->assertStringContainsString("Keep me logged in", $client->getResponse()->getContent());
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirection());
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals(1, $crawler->filter('a:contains("Logout")')->count());
+
+
+
+        // $this->assertResponseIsSuccessful();
+        // $this->assertSelectorTextContains('h1', 'Hello');
+
+        // $link = $crawler->filter("a:contains('Have an account?')")->link();
+        // $crawler = $client->click($link);
+
+        // $this->assertStringContainsString("Keep me logged in", $client->getResponse()->getContent());
     }
 }
