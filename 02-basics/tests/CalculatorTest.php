bdac6ceabe379323d5d42233d39170d2cac95416
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-08 13:14:56 +0100

    3.104 and 105 Mock Test and Code Coverage Report finished

--- 02-basics/tests/CalculatorTest.php
+++ 02-basics/tests/CalculatorTest.php
@@ -2,15 +2,27 @@
 
 namespace App\Tests;
 
-use App\Services\Calculator;
+use App\Services\PromotionCalculator;
 use PHPUnit\Framework\TestCase;
 
-class CalculatorTest extends TestCase
+class PromotionCalculatorTest extends TestCase
 {
     public function testSomething(): void
     {
-        $cal = new Calculator();
-        $result = $cal->add(1,9);
-        $this->assertEquals(10,$result);
+        $calculator = $this->getMockBuilder(PromotionCalculator::class)
+            ->setMethods(["getPromotionPercentage"])
+            ->getMock();
+
+        $calculator->expects($this->any())
+            ->method('getPromotionPercentage')
+            ->willReturn(20);
+        
+        $result = $calculator->calculatePriceAfterPromotion(1,9);
+        //10 - (10 * 20 / 100) = 8
+        $this->assertEquals(8, $result);
+
+        $result = $calculator->calculatePriceAfterPromotion(10,20,50);
+        //80 - (80 * 20 / 100) = 64
+        $this->assertEquals(64, $result);
     }
 }
