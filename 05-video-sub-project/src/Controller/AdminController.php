c1e2304d1920a7f37b06860bd8898bedb66308ed
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-10 19:35:57 +0100

    125. Make template links working in Symfony - part 4

--- 05-video-sub-project/src/Controller/AdminController.php
+++ 05-video-sub-project/src/Controller/AdminController.php
@@ -14,4 +14,28 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/my_profile.html.twig');
     }
+
+    #[Route('/categories', name: 'categories')]
+    public function categories(): Response
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    #[Route('/videos', name: 'videos')]
+    public function videos(): Response
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    #[Route('/upload-video', name: 'upload_video')]
+    public function uploadVideo(): Response
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    #[Route('/users', name: 'users')]
+    public function users(): Response
+    {
+        return $this->render('admin/users.html.twig');
+    }
 }
