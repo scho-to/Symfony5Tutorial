84fb1132d1cf080e92a7f7d9fb55581966069538
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-11 21:04:34 +0100

    6.130. Continue with Category fixtures - part 2

--- 05-video-sub-project/src/DataFixtures/CategoryFixtures.php
+++ 05-video-sub-project/src/DataFixtures/CategoryFixtures.php
@@ -11,7 +11,12 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $this->loadMainCategories($manager);
-        $this->loadSubcategories($manager, 'Electronics', 1);
+        $this->loadElectronics($manager);
+        $this->loadComputers($manager);
+        $this->loadLaptops($manager);
+        $this->loadBooks($manager);
+        $this->loadMovies($manager);
+        $this->loadRomance($manager);
     }
 
     private function loadMainCategories($manager): void
@@ -24,6 +29,36 @@ class CategoryFixtures extends Fixture
         }
 
         $manager->flush();
+    }
+
+    private function loadElectronics($manager): void
+    {
+        $this->loadSubcategories($manager, 'Electronics', 1);
+    }
+
+    private function loadComputers($manager): void
+    {
+        $this->loadSubcategories($manager, 'Computers', 6);
+    }
+
+    private function loadLaptops($manager): void
+    {
+        $this->loadSubcategories($manager,'Laptops',8);
+    }
+    
+    private function loadBooks($manager): void
+    {
+        $this->loadSubcategories($manager,'Books',3);
+    }
+    
+    private function loadMovies($manager): void
+    {
+        $this->loadSubcategories($manager,'Movies',4);
+    }
+    
+    private function loadRomance($manager): void
+    {
+        $this->loadSubcategories($manager,'Romance',18);
     }
 
     private function loadSubcategories($manager, $categoryName, $parentId): void
@@ -57,6 +92,54 @@ class CategoryFixtures extends Fixture
             ['Cameras', 5],
             ['Computers', 6],
             ['Cell Phones', 7]
+        ];
+    }
+
+    private function getComputersData(): array
+    {
+        return [
+            ['Laptops', 8],
+            ['Desktops', 9]
+        ];
+    }
+
+    private function getLaptopsData(): array
+    {
+        return [
+
+            ['Apple',10],
+            ['Asus',11], 
+            ['Dell',12], 
+            ['Lenovo',13], 
+            ['HP',14]
+
+        ];
+    }
+
+
+    private function getBooksData(): array
+    {
+        return [
+            ['Children\'s Books',15],
+            ['Kindle eBooks',16], 
+        ];
+    }
+
+
+    private function getMoviesData(): array
+    {
+        return [
+            ['Family',17],
+            ['Romance',18], 
+        ];
+    }
+
+
+    private function getRomanceData(): array
+    {
+        return [
+            ['Romantic Comedy',19],
+            ['Romantic Drama',20], 
         ];
     }
 }
