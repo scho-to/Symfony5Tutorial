ad81e0f392df152e3ac7a0f51cf9de33dcfc1f7b
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-13 12:32:11 +0100

    7.144. Unit testing of categories - part 1

--- 05-video-sub-project/migrations/Version20220113113148.php
+++ 05-video-sub-project/migrations/Version20220113113148.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220113081848 extends AbstractMigration
+final class Version20220113113148 extends AbstractMigration
 {
     public function getDescription(): string
     {
