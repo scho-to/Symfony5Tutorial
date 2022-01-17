04f27e6c1684676c1f6a337c6077a459673ac033
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2022-01-17 11:02:12 +0100

    8.150. Functional testing of categories - part 2

--- 05-video-sub-project/tests/Controllers/AdminControllerCategoriesTest.php
+++ 05-video-sub-project/tests/Controllers/AdminControllerCategoriesTest.php
@@ -2,18 +2,34 @@
 
 namespace App\Tests\Controllers;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AdminControllerCategoriesTest extends WebTestCase
 {
-
     private ?KernelBrowser $client = null;
+    private ?EntityManager $entityManager = null;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->client = static::createClient();
+        $this->client->disableReboot();
+
+        $this->entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        $this->entityManager->beginTransaction();
+        $this->entityManager->getConnection()->setAutoCommit(false);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        //$this->entityManager->rollback();
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
     }
 
     public function testTextOnPage(): void
@@ -21,5 +37,27 @@ class AdminControllerCategoriesTest extends WebTestCase
         $crawler = $this->client->request('GET', '/admin/categories');
         $this->assertSame('Categories list', $crawler->filter('h2')->text());
         $this->assertStringContainsString('Electronics', $this->client->getResponse()->getContent());
+    }
+
+    public function testNumberOfItems(): void
+    {
+        $crawler = $this->client->request('GET', '/admin/categories');
+        $this->assertCount(21, $crawler->filter('option'));
+    }
+
+    public function testNewCategory(): void
+    {
+        $crawler = $this->client->request('GET', '/admin/categories');
+
+        $form = $crawler->selectButton('Add')->form([
+            'category[parent]' => 1,
+            'category[name]' => 'Other electronics',
+        ]);
+        $this->client->submit($form);
+
+        $category = $this->entityManager->getRepository(Category::class)->findOneBy(['name' => 'Other electronics']);
+
+        $this->assertNotNull($category);
+        $this->assertSame('Other electronics', $category->getName());
     }
 }
